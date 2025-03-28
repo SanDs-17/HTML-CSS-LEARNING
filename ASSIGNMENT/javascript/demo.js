41d754b59ec5ff0352483030e1981cc21926e4ef
SanDs-17 <srisanthosh057@gmail.com>
2025-03-28 12:39:53 +0530

    Added Assignment files

---
+++ ASSIGNMENT/javascript/demo.js
@@ -0,0 +1,12 @@
+function Arithmetic(){
+    var num1= Number(document.getElementById("Input1").value)
+    var num2= Number(document.getElementById("Input2").value)
+    var Add=num1+num2
+    var Sub=num1-num2
+    var Mul=num1*num2
+    var Div=num1/num2
+    alert(Add)
+    alert(Sub)
+    alert(Mul)
+    alert(Div)
+}
